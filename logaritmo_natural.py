f086903e87806c1711344aa3c2da413d68a88dcf
THOMÁS GAJEWSKI STOIANOV <54532962838@info.local>
2026-03-04 21:58:14 -0300

    atualizando

--- logaritmo_natural.py
+++ logaritmo_natural.py
@@ -11,3 +11,19 @@ def Euler():
         e += 1/fatorial(n) 
     return e 
 
+def ln(x, termos=50):
+    if x <= 0:
+        return "Logaritmo indefinido"
+
+    y = x - 1  # porque usamos ln(1 + y)
+
+    resultado = 0
+
+    for n in range(1, termos + 1):
+        termo = ((-1)**(n+1)) * (y**n) / n
+        resultado += termo
+
+    return resultado
+
+
+print(ln(2))
